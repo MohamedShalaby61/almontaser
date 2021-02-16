cb4c9ff1a9dc358d057cf413de5cdfc66c568c39
Abdelrahman Osama <eslamboully@gmail.com>
2021-02-16 22:18:27 +0200

    update

--- resources/lang/en/main.php
+++ resources/lang/en/main.php
@@ -3,5 +3,20 @@
 return array(
   'home' => 'Home Page',
   'language' => 'Language',
-  'aside_language' => 'aside language'
+  'aside_language' => 'aside language',
+    'About Us' => 'About Us',
+    'Our Services' => 'Our Services',
+    'our-advantage' => 'Our Advantage',
+    'our-advantage-desc' => 'Al-Muntasir Limousine Company owns a distinguished fleet of the latest types of cars and we have the best drivers and trained staff at the highest level of efficiency and good morals and holders of certified certificates in safe driving are keen to choose the best way to arrive on time',
+    'Customer service at all times' => 'Customer service at all times',
+    'For reservations' => 'For reservations',
+    'bank_trans' => 'Bank Transactions',
+    'Send Message' => 'Send Message',
+    'name' => 'Name',
+    'email' => 'Email',
+    'phone' => 'Phone',
+    'message' => 'Message',
+    'address' => 'Address',
+    'all_fields' => 'All Fields Must Be not Empty',
+    'message_success' => 'Message Sent Successfully',
 );
